98d36ffdc3ec11dba29d280d1c07435fb8e82bdd
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2022-07-26 10:40:03 +0530

    updates

--- ipfs_upload.py
+++ ipfs_upload.py
@@ -1,28 +1,22 @@
-from ipfshttpclient.client import connect
-import io
-import ipfshttpclient
-#from storage_methods.abstract_storage import Storage
-
+import ipfsApi
 
 class IPFSStorage():
-    def __init__(self, connection_string: str):
-        self.ip = ip
-        self.port = port
-        self.client = ipfshttpclient.connect(connection_string)
-
-    def store(self, filename,content):
-        multihash = self.client.add(io.BytesIO(content))
-        return multihash["Hash"]
+    def __init__(self, ip:str, port=5001):
+        self.client = ipfsApi.Client(ip, port)
+    def store(self, filename):
+        with open(filename, 'rb') as f:
+            multihash = self.client.add(f)
+            return multihash["Hash"]
     def retrieve(self,multihash):
         return self.client.get(multihash)
-    def remove(self,filename):
-        return
-
 
 
 if __name__ == "__main__":
-    ipfs_storage_1 = IPFSStorage("/ip4/<node-ip>/tcp/5001")
-    ipfs_storage_2 = IPFSStorage("/ip4/<node-ip>/tcp/5001")
+    ipfs_storage_1 = IPFSStorage("ip1")
+    ipfs_storage_2 = IPFSStorage("ip2")
+    hash_test = ipfs_storage_1.store("test.txt")
+    print(hash_test)
+    print(ipfs_storage_1.retrieve(hash_test))
     hash_1_a = ipfs_storage_1.store("a")
     hash_1_b = ipfs_storage_1.store("b")
     hash_1_c = ipfs_storage_1.store("c")
@@ -41,7 +35,9 @@ if __name__ == "__main__":
         f.write(hash_2_b + "\n")
         f.write(hash_2_c + "\n")
         f.write(hash_2_d + "\n")
-
-
-
-
+    # read hashes from file
+    with open("hashes.txt", "r") as f:
+        hashes = f.readlines()
+    # retrieve hashes from storage
+    for hash in hashes:
+        print(ipfs_storage_1.retrieve(hash))
